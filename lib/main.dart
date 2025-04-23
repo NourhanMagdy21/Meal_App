defc1a5bdcdbed642997701841a7267c3ac5afa1
NourhanMagdy21 <150091752+NourhanMagdy21@users.noreply.github.com>
2025-04-23 16:57:13 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:food_app/screens/categories_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(
+            brightness: Brightness.dark,
+            seedColor: Color.fromARGB(223, 190, 43, 32)),
+        useMaterial3: true,
+        textTheme: GoogleFonts.latoTextTheme(),
+      ),
+      home: CategoriesScreen(),
+    );
+  }
+}
---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import '../data/dummy_data.dart';
+import '../widgets/category_grid_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Center(child: Text('Meal App')),),
+      body: GridView(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2,
+        mainAxisSpacing: 20,
+        crossAxisSpacing: 20,
+        childAspectRatio: 3/2,
+      ),
+        children: [
+          for(final category in availableCategories)
+            CategoryGridItem(category: category)
+
+        ],),
+    );
+  }
+}
