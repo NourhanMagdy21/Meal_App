010739d6128bdedd8a9a8cf18b246bbd7fc5f283
NourhanMagdy21 <150091752+NourhanMagdy21@users.noreply.github.com>
2025-04-25 20:48:04 +0300

    added some files

---
+++ lib/screens/meals_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MealsScreen extends StatelessWidget {
+  const MealsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text('Meals')),
+      ),
+    );
+  }
+}
--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_app/screens/meals_screen.dart';
 
 import '../models/category.dart';
 
@@ -9,13 +10,24 @@ class CategoryGridItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        padding: EdgeInsets.all(16),
-        decoration: BoxDecoration(
-            gradient: LinearGradient(colors: [
-              category.color.withOpacity(0.5),
-              category.color.withOpacity(0.9),
-            ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
-        child: Text(category.title));
+    return InkWell(
+        onTap: (){
+          Navigator.of(context).push(MaterialPageRoute(builder: (ctx)=> MealsScreen()));
+        },
+
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(16),
+      child: Container(
+          padding: EdgeInsets.all(16),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16),
+              gradient: LinearGradient(colors: [
+                category.color.withOpacity(0.5),
+                category.color.withOpacity(0.9),
+              ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+          child: Text(category.title, style: Theme.of(context).textTheme.titleLarge!.copyWith(
+            color: Theme.of(context).colorScheme.onSurface,
+          ),)),
+    );
   }
 }
