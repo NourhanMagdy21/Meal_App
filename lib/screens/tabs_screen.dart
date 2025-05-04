6d060cf5a7b8c6a17980af8ff3d895df9933d3f8
NourhanMagdy21 <150091752+NourhanMagdy21@users.noreply.github.com>
2025-05-04 10:02:31 +0300

    final update

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:food_app/providers/favorites_provider.dart';
 import 'package:food_app/providers/filters_provider.dart';
-import 'package:food_app/providers/meals_provider.dart';
 import 'package:food_app/providers/nav_bar_provider.dart';
 import 'package:food_app/screens/categories_screen.dart';
 import 'package:food_app/screens/filters_screen.dart';
@@ -10,45 +9,31 @@ import 'package:food_app/screens/meals_screen.dart';
 import '../models/meal.dart';
 import '../widgets/main_drawer.dart';
 
-class TabsScreen extends ConsumerStatefulWidget {
+class TabsScreen extends ConsumerWidget {
   const TabsScreen({super.key});
 
   @override
-  ConsumerState<TabsScreen> createState() => _TabsScreenState();
-}
 
-class _TabsScreenState extends ConsumerState<TabsScreen> {
-  int _selectPageIndex = 0;
-
-  void _setScreen(String identifier) {
-    Navigator.of(context).pop();
-    if (identifier == 'filters') {
-      Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (ctx) => FiltersScreen(),
-        ),
-      );
-    }
-  }
 
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
 final availableMeals = ref.watch(filteredMealsProvider);
+final selectPageIndex= ref.watch(navBarProvider);
 
     Widget activePage = CategoriesScreen(
       availableMeals: availableMeals,
     );
     var activePageTitle = 'Pick Your Meal';
-    if (_selectPageIndex == 1) {
+
+    if (selectPageIndex == 1) {
       final List<Meal> favoriteMeal = ref.watch(favoritesMealsProvider);
       activePage = MealsScreen(
         meals: favoriteMeal,
       );
       activePageTitle = 'Favorites';
     }
-ref.watch(navBarProvider);
+
     
     return Scaffold(
       appBar: AppBar(
@@ -56,11 +41,20 @@ ref.watch(navBarProvider);
       ),
       body: activePage,
       drawer: MainDrawer(
-        onSelectScreen: _setScreen,
+        onSelectScreen: (String identifier) {
+  Navigator.of(context).pop();
+  if (identifier == 'filters') {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (ctx) => FiltersScreen(),
+      ),
+    );}}
+
       ),
       bottomNavigationBar: BottomNavigationBar(
-          // onTap: _selectPage,
-          currentIndex: _selectPageIndex,
+          onTap:ref.read(navBarProvider.notifier).selectPage,
+          currentIndex: selectPageIndex,
           items: [
             BottomNavigationBarItem(
                 icon: Icon(Icons.set_meal), label: 'Categories'),
